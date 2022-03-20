9778f80974904a40ec9a257d6dd6d67e5d371507
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-20 16:48:41 -0700

    Implemented deleteUser and updated updateUser method

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,18 +31,28 @@ module.exports = {
   },
   /** PUT to update a user by its _id */
   updateUser(req, res) {
-    User.findOneAndUpdate(
-      { _id: req.params.userId },
+    User.findByIdAndUpdate(
+      req.params.userId,
       { $set: req.body },
       { runValidators: true, new: true }
     )
-      .then((user) => res.json(user))
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that id" })
+          : res.json(user)
+      )
       .catch((err) => res.status(500).json(err));
   },
   /** DELETE to remove user by its _id */
   // bonus: remove a user's associated thoughts when deleted
   deleteUser(req, res) {
-    res.send("Unimplemented");
+    User.findByIdAndDelete(req.params.userId)
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that id" })
+          : res.json({ user, message: `Deleted user ${user.id}`})
+      )
+      .catch((err) => res.status(500).json(err));
   },
   /** POST to add a new friend to a user's friend list */
   addFriend(req, res) {
