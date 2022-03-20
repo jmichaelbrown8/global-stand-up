d7e19289011166840c4a4eec127902755a4d4c63
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-20 14:43:53 -0700

    Added descriptions to swagger.yaml and turned comments into jsdoc in controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,33 +1,34 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  // GET to get all thoughts
+  /** GET to get all thoughts */
   getThoughts(req, res) {
     Thought.find()
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-  // GET to get a single thought by its _id
+  /** GET to get a single thought by its _id */
   getSingleThought(req, res) {
     res.send("Unimplemented");
   },
-  // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  /** POST to create a new thought */
+  // (don't forget to push the created thought's _id to the associated user's thoughts array field)
   createThought(req, res) {
     res.send("Unimplemented");
   },
-  // PUT to update a thought by its _id
+  /** PUT to update a thought by its _id */
   updateThought(req, res) {
     res.send("Unimplemented");
   },
-  // DELETE to remove a thought by its _id
+  /** DELETE to remove a thought by its _id */
   deleteThought(req, res) {
     res.send("Unimplemented");
   },
-  // POST to create a reaction stored in a single thought's reactions array field
+  /** POST to create a reaction stored in a single thought's reactions array field */
   addReaction(req, res) {
     res.send("Unimplemented");
   },
-  // DELETE to pull and remove a reaction by the reaction's reactionId value
+  /** DELETE to pull and remove a reaction by the reaction's reactionId value */
   removeReaction(req, res) {
     res.send("Unimplemented");
   },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,34 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  // GET all users
+  /** GET all users */
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  // GET a single user by its _id and populated thought and friend data
+  /** GET a single user by its _id and populated thought and friend data */
   getSingleUser(req, res) {
     res.send("Unimplemented");
   },
-  // POST a new user
+  /** POST a new user */
   createUser(req, res) {
     res.send("Unimplemented");
   },
-  // PUT to update a user by its _id
+  /** PUT to update a user by its _id */
   updateUser(req, res) {
     res.send("Unimplemented");
   },
-  // DELETE to remove user by its _id
+  /** DELETE to remove user by its _id */
   // bonus: remove a user's associated thoughts when deleted
   deleteUser(req, res) {
     res.send("Unimplemented");
   },
-  // POST to add a new friend to a user's friend list
+  /** POST to add a new friend to a user's friend list */
   addFriend(req, res) {
     res.send("Unimplemented");
   },
-  // DELETE to remove a friend from a user's friend list
+  /** DELETE to remove a friend from a user's friend list */
   removeFriend(req, res) {
     res.send("Unimplemented");
   },
